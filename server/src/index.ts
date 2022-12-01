88b251bbfc0d821f3c256e9629070d21d4cdc7c4
HallowedSteek <warman643@gmail.com>
2022-12-01 22:26:42 +0200

    added .env port

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,7 +14,7 @@ import cardForDeck from "./controller/cardsForDeck";
 import deleteCard from "./controller/deleteCard";
 import getCards from "./controller/getCards";
 
-const PORT: number = 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
